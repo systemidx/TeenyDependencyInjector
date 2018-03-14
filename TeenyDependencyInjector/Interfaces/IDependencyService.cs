b654d9bcd3fd765d37e1c959c5e8c70d84bd7dab
Bryon Weber <bweber@connectionseducation.com>
2018-03-13 22:47:37 -0400

    Updates comments

--- TeenyDependencyInjector/Interfaces/IDependencyService.cs
+++ TeenyDependencyInjector/Interfaces/IDependencyService.cs
@@ -5,7 +5,7 @@ namespace TeenyDependencyInjector.Interfaces
     public interface IDependencyService
     {
         /// <summary>
-        /// Registers the instance asynchronous.
+        /// Registers the instance asynchronously.
         /// </summary>
         /// <typeparam name="TInterface">The type of the interface.</typeparam>
         /// <typeparam name="TConcrete">The type of the concrete.</typeparam>
@@ -14,9 +14,28 @@ namespace TeenyDependencyInjector.Interfaces
         /// <returns></returns>
         Task<TInterface> RegisterInstanceAsync<TInterface, TConcrete>(TInterface instance, string name = null) where TConcrete: class, TInterface;
 
+        /// <summary>
+        /// Registers the type asynchronously.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="TConcrete">The type of the concrete.</typeparam>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns></returns>
         Task RegisterTypeAsync<TInterface, TConcrete>(params object[] parameters);
 
+        /// <summary>
+        /// Creates the instance asynchronously.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <returns></returns>
         Task<TInterface> CreateInstanceAsync<TInterface>() where TInterface : class;
+
+        /// <summary>
+        /// Gets the instance asynchronously.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <param name="bindingName">Name of the binding.</param>
+        /// <returns></returns>
         Task<TInterface> GetInstanceAsync<TInterface>(string bindingName = null) where TInterface : class;
     }
 }
